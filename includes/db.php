7bc96f90e327d9c5ef51d2cce9271f5f6444ecce
Falthawadi59 <fatimaalthawadi59@gmail.com>
2024-12-09 16:03:56 +0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,3 +1,10 @@
+<?php 
+// Database configuration
+$host = 'localhost';
+$dbname = 'uob_room_booking'; // Correct database name
+$username = 'root';
+$password = ''; // Your MySQL password, if set
+
 <?php
 try {
     $pdo = new PDO("mysql:host=localhost;dbname=uob_room_booking", "root", "");
